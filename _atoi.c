32022e26cc5c9810a13764312055b85fd8b056dc
kingStaff <karimawudulai@gmail.com>
2023-05-17 06:15:53 -0700

    update _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -1,37 +1,36 @@
 #include "shell.h"
 
 /**
-
-is_interactive - Checks if the shell is running in interactive mode.
-@info: Pointer to a struct containing shell information.
-Return: 1 if the shell is in interactive mode, 0 otherwise.
-*/
+ * interactive - Checks if the shell is running in interactive mode.
+ * @info: Pointer to a struct containing shell information.
+ * Return: 1 if the shell is in interactive mode, 0 otherwise.
+ */
 int interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
-
-_isdelim - Checks if a character is a delimiter.
-@c: The character to check.
-@delimiters: The string of delimiters.
-Return: 1 if the character is a delimiter, 0 otherwise.
-*/
+ * _isdelim - Checks if a character is a delimiter.
+ * @c: The character to check.
+ * @delim: The string of delimiters.
+ * Return: 1 if the character is a delimiter, 0 otherwise.
+ */
 int _isdelim(char c, char *delim)
 {
 	while (*delim)
+	{
 		if (*delim++ == c)
 			return (1);
+	}
 	return (0);
 }
 
 /**
-
-_isalphab - Checks if a character is alphabetic.
-@c: The character to check.
-Return: 1 if the character is alphabetic, 0 otherwise.
-*/
+ * _isalpha - Checks if a character is alphabetic.
+ * @c: The character to check.
+ * Return: 1 if the character is alphabetic, 0 otherwise.
+ */
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
@@ -41,10 +40,10 @@ int _isalpha(int c)
 }
 
 /**
-_atoi - Converts a string to an integer.
-@s: The string to be converted.
-Return: 0 if no numbers in the string, the converted number otherwise.
-*/
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ * Return: 0 if no numbers in the string, the converted number otherwise.
+ */
 int _atoi(char *s)
 {
 	int i, sign = 1, flag = 0, output;
@@ -62,7 +61,9 @@ int _atoi(char *s)
 			result += (s[i] - '0');
 		}
 		else if (flag == 1)
+		{
 			flag = 2;
+		}
 	}
 
 	if (sign == -1)
